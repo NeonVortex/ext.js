5ba7040b9ee59cc392dc1d728ac27abf102c13f8
Zhiq Zhao <zhiq.zhao@ericsson.com>
2018-02-13 15:23:20 -0500

    Init

--- test/co-test.js
+++ test/co-test.js
@@ -2,6 +2,6 @@ const co = require("../co/co");
 const fs = require("../fs-async/fs-async");
 
 co(function*(){
-    let fileText = yield fs.readFileAsync('README.md');
+    let fileText = yield fs.readFileAsync('test/co-test.js');
     console.log(fileText.toString("utf-8"));
 });
