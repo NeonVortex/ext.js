c066224b880757eb38fb9952f233c28a8b14d6f8
texasbruce <bruce@texasbruce.com>
2018-03-25 05:04:42 -0400

    Added EPLISON and equals for floating numbers

--- Number.js
+++ Number.js
@@ -1,7 +1,15 @@
+if(Number.EPSILON == undefined) {
+    Number.EPSILON = 2.220446049250313e-16;
+};
+
 Number.prototype.times = function (fn) {
     for (let i = 0; i < this; i++) {
         fn(i, this, this);
     }
+};
+
+Number.equals = (left, right) => {
+    return Math.abs(left-right) < Number.EPSILON;
 };
 
 //(3).times ((i)=>console.log(i));
